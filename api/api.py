2115949b1e99fcaa5cd2490a79e427ae8b8395ca
Marcus Vinicius Thomas <TH@gsokoeln.onmicrosoft.com>
2021-11-24 20:30:23 +0100

    Erster Test erfolgreich!

--- api/api.py
+++ api/api.py
@@ -1,10 +1,11 @@
 import flask
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = true
+app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
         return "<h1>FLASK Showcase</h1><p>Diese Seite ist eine Testumgebung zur Entwicklung eines Web-Backends mit Python und Flask.</p>"
+
 
 app.run()
