ca4c5a62b348d84acd36bf731473a76ad2fe6584
Marcus Vinicius Thomas <TH@gsokoeln.onmicrosoft.com>
2021-11-24 21:15:56 +0100

    Die API ist nun bereit, um erste Abfragen an die Datenbank vorzunehmen. Beispielhafte Aufrufe via:
    http://127.0.0.1:5000/api/v1/resources/books/all
    
    oder
    
    http://127.0.0.1:5000/api/v1/chinook/customers?CustomerId=1

--- api/api.py
+++ api/api.py
@@ -6,56 +6,85 @@ import sqlite3
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True #Zeigt Fehlerinformationen im Browser, statt nur einer generischen Error-Message
 
-# Create some test data for our catalog in the form of a list of dictionaries.
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
-
 @app.route('/', methods=['GET'])
 def home():
         return "<h1>FLASK Showcase</h1><p>Diese Seite ist eine Testumgebung zur Entwicklung eines Web-Backends mit Python und Flask.</p>"
 
+# @app.route('/api/v1/resources/books', methods=['GET'])
+# def api_id():
+#     # Check if an ID was provided as part of the URL.
+#     # If ID is provided, assign it to a variable.
+#     # If no ID is provided, display an error in the browser.
+#     if 'id' in request.args:
+#         id = int(request.args['id'])
+#     else:
+#         return "Error: No id field provided. Please specify an id."
+#
+#     # Create an empty list for our results
+#     results = []
+#
+#     # Loop through the data and match results that fit the requested ID.
+#     # IDs are unique, but other fields might return many results
+#     for book in books:
+#         if book['id'] == id:
+#             results.append(book)
+#
+#     # Use the jsonify function from Flask to convert our list of
+#     # Python dictionaries to the JSON format.
+#     return jsonify(results)
 
-# A route to return all of the available entries in our catalog.
-@app.route('/api/v1/resources/books/all', methods=['GET'])
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+@app.route('/api/v1/chinook/customers/all', methods=['GET'])
 def api_all():
-    return jsonify(books)
+    conn = sqlite3.connect('../chinook.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    all_customers = cur.execute('SELECT * FROM customers;').fetchall()
 
-@app.route('/api/v1/resources/books', methods=['GET'])
-def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
+    return jsonify(all_customers)
 
-    # Create an empty list for our results
-    results = []
 
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
-    for book in books:
-        if book['id'] == id:
-            results.append(book)
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
+
+@app.route('/api/v1/chinook/customers', methods=['GET'])
+def api_filter():
+    query_parameters = request.args
+
+    CustomerId = query_parameters.get('CustomerId')
+    LastName = query_parameters.get('LastName')
+    Country = query_parameters.get('Country')
+
+    query = "SELECT * FROM customers WHERE"
+    to_filter = []
+
+    if CustomerId:
+        query += ' CustomerId=? AND'
+        to_filter.append(CustomerId)
+    if LastName:
+        query += ' LastName=? AND'
+        to_filter.append(LastName)
+    if Country:
+        query += ' Country=? AND'
+        to_filter.append(Country)
+    if not (CustomerId or LastName or Country):
+        return page_not_found(404)
+
+    query = query[:-4] + ';'
+
+    conn = sqlite3.connect('../chinook.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+
+    results = cur.execute(query, to_filter).fetchall()
+
     return jsonify(results)
 
 app.run()
