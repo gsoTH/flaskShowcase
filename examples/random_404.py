04caa6621df5ef62d3440ece04d80c4db7860984
Marcus Vinicius Thomas <TH@gsokoeln.onmicrosoft.com>
2021-11-27 23:34:07 +0100

    Liste von Fehlermeldungen.

--- examples/random_404.py
+++ examples/random_404.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.errorhandler(404)
 def page_not_found(e):
+
+    errorMessageList = ["Whoops, this page doesn't exist.",
+                        "There's nothing here.",
+                        "There's been a glitch",
+                        "404 - Feel free to dance your frustration off",
+                        "Four hundred and fourth message of error"]
+
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
 
 if __name__ == "__main__":
     app.run()
