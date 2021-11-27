8e95e5b98bdd5c788cd4642c9f63dd767dc96bcc
Marcus Vinicius Thomas <TH@gsokoeln.onmicrosoft.com>
2021-11-27 23:24:52 +0100

    Custom 404
    When an error occurs in Flask, an appropriate HTTP status code will be returned. 400-499 indicate errors with the client’s request data, or about the data requested. 500-599 indicate errors with the server or application itself.

--- examples/random_404.py
+++ examples/random_404.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
 if __name__ == "__main__":
     app.run()
