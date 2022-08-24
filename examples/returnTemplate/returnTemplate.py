5e8999fecab0e5d91604d656383dfc2a6cb09d35
M.Thomas <m.thomas@gso.schule.koeln>
2022-08-24 15:22:22 +0200

    Quelle wird nun auch zurückgegeben

--- examples/returnTemplate/returnTemplate.py
+++ examples/returnTemplate/returnTemplate.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template
 
 
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    aktuellerSpruch = getSpruch();
-    return render_template('index.html', spruch = aktuellerSpruch, quelle = "")
+    aktuellerSpruch, aktuelleQuelle = getSpruch();
+    return render_template('index.html', spruch = aktuellerSpruch, quelle = aktuelleQuelle)
 
 def getSpruch():
-    return "Nun guck‘ ihn dir an, hat auch nicht mehr Hirn als ’n Spatz Fleisch an der Kniescheibe.";
+    return "Nun guck‘ ihn dir an, hat auch nicht mehr Hirn als ’n Spatz Fleisch an der Kniescheibe.", "Terence, Zwei wie Pech und Schwefel"
+
+
+
 
 if __name__ == "__main__":
     app.run()
