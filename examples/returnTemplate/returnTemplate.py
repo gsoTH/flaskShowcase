84f9242bba953183ddfda67c6a1f7de975b562e0
M.Thomas <m.thomas@gso.schule.koeln>
2022-08-25 10:44:24 +0200

    Spruchnummer kann übergeben werden

--- examples/returnTemplate/returnTemplate.py
+++ examples/returnTemplate/returnTemplate.py
@@ -1,15 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 import sqlite3
+import random
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/', methods=['GET'])
 def index():
-    aktuellerSpruch, aktuelleQuelle = getSpruch();
+    query_parameters = request.args
+    spruchNr = query_parameters.get('nr')
+    if spruchNr:
+        aktuellerSpruch, aktuelleQuelle = getSpruch(spruchNr)
+    else:
+        aktuellerSpruch, aktuelleQuelle = getRandomSpruch()
+    
     return render_template('index.html', spruch = aktuellerSpruch, quelle = aktuelleQuelle)
 
-def getSpruch(nummer: int = 1):
+
+def getAnzahlSprueche():
+    query = "SELECT count(*) FROM zitate"
+
+    conn = sqlite3.connect('./giganten.sqlite')
+    result = conn.execute(query).fetchone()
+
+    return int(result[0]) #Ohne [0] --> TypeError: int() argument must be a string, a bytes-like object or a number, not 'tuple'
+
+def getRandomSpruch():
+    anzahlSprueche = getAnzahlSprueche
+    zufallsNummer = random.randrange(1, anzahlSprueche)
+
+    spruch = getSpruch(zufallsNummer)
+
+    return spruch[0], spruch[1]
+
+
+def getSpruch():
     query = "SELECT spruch, quelle FROM zitate WHERE nr = "
     query = query + str(nummer)
 
