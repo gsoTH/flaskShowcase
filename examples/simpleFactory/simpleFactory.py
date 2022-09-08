c2b81132f4797792edaad18091be2372ac1d8bb4
M.Thomas <m.thomas@gso.schule.koeln>
2022-09-08 12:21:57 +0200

    Factory-Pattern jetzt deutlich

--- examples/simpleFactory/simpleFactory.py
+++ examples/simpleFactory/simpleFactory.py
@@ -1,12 +1,17 @@
 from flask import Flask
-import ausgelagerteRoute # Hier wird der Inhalt von ausgelagerteRoute.py verfügbar gemacht
+import ausgelagerteRoute                        # macht ausgelagerteRoute.py verfügbar
 
-instance = Flask(__name__)
-# Statt instance steht hier üblicherweise app
-# beides bezeichnet eine Instanz von Flask, also den gestarteten Server
 
-ausgelagerteRoute.initialisieren(instance)
+def create_app():                               # Factory-Pattern
+
+    instance = Flask(__name__)                  # Statt instance steht hier üblicherweise app
+                                                # beides bezeichnet eine Instanz von Flask, also den gestarteten Server
+
+    ausgelagerteRoute.initialisieren(instance)  # Auslagern nicht zwingend nötig, aber ohne Factory-Pattern unmöglich
+
+    return instance
 
 
 if __name__ == "__main__":
-    instance.run()
+    app = create_app()
+    app.run()
