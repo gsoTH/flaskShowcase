0db1f299e40746ca8671057d7cbb683273e84a18
M. Thomas <m.thomas@gso.schule.koeln>
2023-11-30 23:10:58 +0100

    Klarstellung

--- datenbanken/sqlite_row_factory.py
+++ datenbanken/sqlite_row_factory.py
@@ -9,77 +9,97 @@
 
 import sqlite3
 
-con: sqlite3.Connection = sqlite3.connect(":memory:")
-create_sql: str = """
-    CREATE TABLE test(
-        id          INTEGER
-    ,   vorname     TEXT
-    ,   nachname    TEXT
-    ,   PRIMARY KEY(id)
-    );
-    
-    INSERT INTO test VALUES 
-        (1, 'Donald','Duck')
-    ,   (2, 'Mickey','Maus')
-    ,   (3, 'Balou', 'Bär' )
-    ;
-"""
-con.executescript(create_sql)
+def init_db() -> sqlite3.Connection:
+    """Erzeugt eine in-memory-Datenbank mit Testdaten"""
+    con: sqlite3.Connection = sqlite3.connect(":memory:")
+    create_sql: str = """
+        CREATE TABLE test(
+            id          INTEGER
+        ,   vorname     TEXT
+        ,   nachname    TEXT
+        ,   PRIMARY KEY(id)
+        );
+        
+        INSERT INTO test VALUES 
+            (1, 'Donald','Duck')
+        ,   (2, 'Mickey','Maus')
+        ,   (3, 'Balou', 'Bär' )
+        ;
+    """
+    con.executescript(create_sql)
+
+    return con
+
+
+def get_cursor() -> sqlite3.Cursor:
+    """Ein cursor definiert ein Ergebnis-Set"""
+    con = init_db()
+    cursor: sqlite3.Cursor = con.execute("SELECT * FROM test")
+    con.close()
+    return cursor
 
 
 
-sql: str = "SELECT * FROM test"
 
-# Quelle: 1
-# By default, sqlite3 represents each row as a tuple.
-# row_factory auf default Wert setzen
-# Jede Row wird nun als Python-Tuple ausgegeben
-cursor: sqlite3.Cursor = con.execute(sql)
-cursor.row_factory = None
-print(cursor.fetchall())
+def default_row_factory():
+    # Quelle: 1
+    # By default, sqlite3 represents each row as a tuple.
+    # row_factory auf default Wert setzen
+    # Jede Row wird nun als Python-Tuple ausgegeben
+    cursor: sqlite3.Cursor = get_cursor()
+    cursor.row_factory = None
+    print(cursor.fetchall())
 
-# Ergebnis:
-#   [
-#       (1, 'Donald', 'Duck'),
-#       (2, 'Mickey', 'Maus'),
-#       (3, 'Balou', 'Bär')
-#   ]
-
-
-# Quelle: 1
-# If a tuple does not suit your needs, you can use the sqlite3.Row class or a custom row_factory.
-def dict_factory(cursor, row) -> dict:
-    """returns each row as a dict, with column names mapped to values"""
-    fields = [column[0] for column in cursor.description]
-    return {key: value for key, value in zip(fields, row)}
-
-cursor: sqlite3.Cursor = con.execute(sql)
-cursor.row_factory = dict_factory
-print(cursor.fetchall())
-
-# Ergebnis:
-#   [
-#       {'id': 1, 'vorname': 'Donald', 'nachname': 'Duck'},
-#       {'id': 2, 'vorname': 'Mickey', 'nachname': 'Maus'},
-#       {'id': 3, 'vorname': 'Balou', 'nachname': 'Bär'}
-#   ]
+    # Ergebnis:
+    #   [
+    #       (1, 'Donald', 'Duck'),
+    #       (2, 'Mickey', 'Maus'),
+    #       (3, 'Balou', 'Bär')
+    #   ]
 
 
-# Quelle: 2
-def dict_factory_short(cursor, row) -> dict:
-    """returns each row as a dict, with column names mapped to values"""
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
+def dict_statt_tuple():
+    # Quelle: 1
+    # If a tuple does not suit your needs, you can use the sqlite3.Row class or a custom row_factory.
+    def dict_factory(cursor, row) -> dict:
+        """returns each row as a dict, with column names mapped to values"""
+        fields = [column[0] for column in cursor.description]
+        return {key: value for key, value in zip(fields, row)}
 
-cursor: sqlite3.Cursor = con.execute(sql)
-cursor.row_factory = dict_factory_short
-print(cursor.fetchall())
+    cursor: sqlite3.Cursor = get_cursor()
+    cursor.row_factory = dict_factory
+    print(cursor.fetchall())
 
-# Ergebnis:
-#   [
-#       {'id': 1, 'vorname': 'Donald', 'nachname': 'Duck'},
-#       {'id': 2, 'vorname': 'Mickey', 'nachname': 'Maus'},
-#       {'id': 3, 'vorname': 'Balou', 'nachname': 'Bär'}
-#   ]
+    # Ergebnis:
+    #   [
+    #       {'id': 1, 'vorname': 'Donald', 'nachname': 'Duck'},
+    #       {'id': 2, 'vorname': 'Mickey', 'nachname': 'Maus'},
+    #       {'id': 3, 'vorname': 'Balou', 'nachname': 'Bär'}
+    #   ]
+
+
+def einfachere_version():
+    # Quelle: 2
+    def dict_factory_short(cursor, row) -> dict:
+        """returns each row as a dict, with column names mapped to values"""
+        d = {}
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
+
+    cursor: sqlite3.Cursor = get_cursor()
+    cursor.row_factory = dict_factory_short
+    print(cursor.fetchall())
+
+    # Ergebnis:
+    #   [
+    #       {'id': 1, 'vorname': 'Donald', 'nachname': 'Duck'},
+    #       {'id': 2, 'vorname': 'Mickey', 'nachname': 'Maus'},
+    #       {'id': 3, 'vorname': 'Balou', 'nachname': 'Bär'}
+    #   ]
+
+
+if __name__ == "__main__":
+    default_row_factory()
+    dict_statt_tuple()
+    einfachere_version()
