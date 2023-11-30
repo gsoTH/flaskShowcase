7108a73c46b9026ebf3d560d1304841a35e0681d
M.Thomas <m.thomas@gso.schule.koeln>
2023-11-30 14:03:18 +0100

    Get-Request jetzt mit query-Parametern

--- unitTests/testFlaskSimple/myAPI.py
+++ unitTests/testFlaskSimple/myAPI.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 
 
-def init_app(app):                          # Normalerweise in eigene Datei ausgelagert, aber Pytest wirft bei mir Import-Fehler :(
-    @app.route("/")
-    def hello_world():
-        return "<h1>Hello, World!</h1>"
+def init_app(app):                                  # Normalerweise in eigene Datei ausgelagert, aber Pytest wirft bei mir Import-Fehler :(
+
+    @app.route("/", methods=['GET'])                # GET ist default, muss nicht angegeben werden
+    def hello_person():                             # POST muss explizit angegeben werden, sonst 405
+        vorname = request.args.get("vorname")
+        nachname = request.args.get("nachname")
+        
+        result = "<h1>Hello, World!</h1>"
+        
+        if vorname and nachname:
+            result = "<h1>Hello, " + vorname + " " + nachname + "</h1>"            
+        
+        return result, 200
 
 
 def create_app():                           # Factory Pattern
--- unitTests/testFlaskSimple/test_myApi.py
+++ unitTests/testFlaskSimple/test_myApi.py
@@ -19,3 +19,17 @@ def test_hello_world(app):
 
     assert response.status_code == 200
     assert b"<h1>Hello, World!</h1>" in response.data   # b wandelt den string in bytecode um, sonst Typfehler
+    
+
+def test_hello_world__with_arguments(app):
+    route = "/"
+    payload = {                                                 # Python-Dictionary
+        "vorname": "Bud",
+        "nachname": "Spencer"
+    }
+    
+    response = app.get(path = route, query_string = payload)   # query_string setzt die Inhalte eines Dicts hinter das ? in der URL
+                                                               # entspricht hier : GET /?vorname=Bud&nachname=Spencer
+
+    assert response.status_code == 200
+    assert b"Hello, Bud Spencer" in response.data
